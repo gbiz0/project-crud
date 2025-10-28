5d7a0f2a7847ece9063559a4db4e3324d8188c06
gbiz0 <jardimbgustavo@gmail.com>
2025-10-28 08:12:13 -0300

    feat(docker): create table

---
+++ init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE
+    Fornecedor (
+        ID SERIAL PRIMARY KEY,
+        Removido BOOLEAN DEFAULT false NOT NULL,
+        NomeFantasia VARCHAR(100) NOT NULL,
+        RazaoSocial VARCHAR(200),
+        CNPJ VARCHAR(18)
+    );
+
+CREATE TABLE
+    ContaPagar (
+        ID SERIAL PRIMARY KEY,
+        Removido BOOLEAN DEFAULT false NOT NULL,
+        Descricao VARCHAR(255) NOT NULL,
+        DataVencimento DATE NOT NULL,
+        Valor DECIMAL(10, 2) NOT NULL,
+        DataPagamento DATE,
+        ValorPago DECIMAL(10, 2),
+        FornecedorID INTEGER NOT NULL,
+        CONSTRAINT fk_fornecedor FOREIGN KEY (FornecedorID) REFERENCES Fornecedor (ID)
+    );
